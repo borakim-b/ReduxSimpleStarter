534a640c0d89a1b4430d5f793d4d39a1e6d7f160
borakim-b <bora.kim.tech@gmail.com>
2018-05-31 15:05:15 +0900

    section 2

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,19 +4,22 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { term: 'Starting Value' };
+		this.state = { term: '' };
 	}
 
 	render() {
 		return (
-			<div>
-				<input value={this.state.term} onChange={event => this.setState({ term: event.target.value })} />
+			<div className="search-bar">
+				<input 
+					value={this.state.term} 
+					onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
 	}
 
-	onInputChange(event) {
-		console.log(event.target.value);
+	onInputChange(term) {
+		this.setState({ term });
+		this.props.onSearchTermChange(term);
 	}
 }
 
